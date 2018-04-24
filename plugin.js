a683e871ad5290c54a7cfb4cc2a2a012682fee89
oleks <oleksandrknyga@gmail.com>
2018-04-24 21:28:56 +0300

    send test email

--- plugin.js
+++ plugin.js
@@ -1,36 +1,44 @@
 const nodemailer = require('nodemailer');
+const {isTest, host, port, secure, user, pass, url} = require('./config');
 
-module.exports = function(user, params, zp) {
-	// see https://nodemailer.com/about/
-	nodemailer.createTestAccount((err, account) => {
-	    let transporter = nodemailer.createTransport({
-	        host: 'smtp.ethereal.email',
-	        port: 587,
-	        secure: false,
-	        auth: {
-	            user: account.user,
-	            pass: account.pass,
-	        }
-	    });
+function sendEmail({host, port, secure, user, pass, url, z, checkUrl}) {
+    const transporter = nodemailer.createTransport({
+        host,
+        port,
+        secure,
+        auth: {user, pass},
+    });
 
-	    const link = 'http://localhost:3000/key/verify/' + encodeURIComponent(zp);
+    const link = `${url}${checkUrl.replace('${z}', encodeURIComponent(z))}`;
 
-	    let mailOptions = {
-	        from: 'AuthMagic',
-	        to: user,
-	        subject: 'Your Magic Link',
-	        text: `Hi friend! Open this link to finish authorization: ${link}`,
-	        html: 'Hi friend! Open this link to finish authorization: <a href="${link}">${link}</a>',
-	    };
+    let mailOptions = {
+        from: 'AuthMagic',
+        to: user,
+        subject: 'Your Magic Link',
+        text: `Hi friend! Open this link to finish authorization: ${link}`,
+        html: `Hi friend! Open this link to finish authorization: <a href="${link}">${link}</a>`,
+    };
 
-	    transporter.sendMail(mailOptions, (error, info) => {
-	        if (error) {
-	            return console.log(error);
-	        }
-	        
-	        console.log('Message sent: %s', info.messageId);
-	        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-	    });
-	});
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
+}
+
+module.exports = async function({user, params, z, config: {checkUrl}}) {
+	if(isTest) {
+        // see https://nodemailer.com/about/
+        nodemailer.createTestAccount((err, {user, pass}) => {
+            if(!err) {
+                sendEmail({host, port, secure, user, pass, url, z, checkUrl});
+            }
+        });
+	}
+
+
 
 };
