000fa88279263aa04213b1d371295d0766be6606
oleks <oleksandrknyga@gmail.com>
2018-04-25 16:41:02 +0300

    indent

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const isTest = typeof process.env.MAIL_TEST === 'undefined' ? false : !!process.env.MAIL_TEST;
 module.exports = {
-	isTest,
-	url: process.env.URL || isTest ? 'http://localhost:3000' : '',
-	mailer: {
-        auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS,
-        },
-        host: process.env.MAIL_HOST || 'smtp.ethereal.email',
-        port: process.env.MAIL_PORT || 587,
-        secure: isTest ? false : typeof process.env.MAIL_SECURE === 'undefined' ? true : !!process.env.MAIL_SECURE,
-	},
+  isTest,
+  url: process.env.URL || isTest ? 'http://localhost:3000' : '',
+  mailer: {
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+    host: process.env.MAIL_HOST || 'smtp.ethereal.email',
+    port: process.env.MAIL_PORT || 587,
+    secure: isTest ? false : typeof process.env.MAIL_SECURE === 'undefined' ? true : !!process.env.MAIL_SECURE,
+  },
 };
--- plugin.js
+++ plugin.js
@@ -2,36 +2,36 @@ const nodemailer = require('nodemailer');
 const {isTest, url, mailer} = require('./config');
 
 function sendEmail({mailer, url, z, config, user, params}) {
-    const transporter = nodemailer.createTransport(mailer);
-    const link = `${url}${config.checkUrl.replace('${z}', encodeURIComponent(z))}`;
-    const mailOptions = {
-        from: 'AuthMagic',
-        to: user,
-        subject: 'Your Magic Link',
-        text: `Hi friend! Open this link to finish authorization: ${link}`,
-        html: `Hi friend! Open this link to finish authorization: <a href="${link}">${link}</a>`,
-    };
+  const transporter = nodemailer.createTransport(mailer);
+  const link = `${url}${config.checkUrl.replace('${z}', encodeURIComponent(z))}`;
+  const mailOptions = {
+    from: 'AuthMagic',
+    to: user,
+    subject: 'Your Magic Link',
+    text: `Hi friend! Open this link to finish authorization: ${link}`,
+    html: `Hi friend! Open this link to finish authorization: <a href="${link}">${link}</a>`,
+  };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
 
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    })
-    ;
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  })
+  ;
 }
 
 module.exports = function ({user, params, z, config}) {
-    if (isTest) {
-        // see https://nodemailer.com/about/
-        nodemailer.createTestAccount((err, auth) => {
-            if (!err) {
-                sendEmail({mailer: {...mailer, auth}, url, z, config, user, params});
-            }
-        });
-    } else {
-        sendEmail({mailer, url, z, config, user, params});
-    }
+  if (isTest) {
+    // see https://nodemailer.com/about/
+    nodemailer.createTestAccount((err, auth) => {
+      if (!err) {
+        sendEmail({mailer: {...mailer, auth}, url, z, config, user, params});
+      }
+    });
+  } else {
+    sendEmail({mailer, url, z, config, user, params});
+  }
 };
